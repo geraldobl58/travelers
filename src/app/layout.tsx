5c014398f477db0ebaa6f33b61f529e2f37fde72
Geraldo Luiz <geraldobl58@gmail.com>
2024-06-10 18:04:30 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+
+import { Poppins } from "next/font/google";
+
+import "./globals.css";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "travelers",
+  description: "Travelers Web Application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
