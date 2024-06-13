23a97748dd940e317d839ced6a9aeeb33acd3751
Geraldo Luiz <geraldobl58@gmail.com>
2024-06-13 16:43:09 -0300

    feat: Add room api

---
+++ src/app/api/room/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+import { auth } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const room = await db.room.create({
+      data: {
+        ...body,
+      },
+    });
+
+    return NextResponse.json(room);
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
+}
